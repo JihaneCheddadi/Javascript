4c0cca4d9f4003ebb56838d7d43a3ffc724de224
Jihane Cheddadi <Jihaneacheddadi@gmail.com>
2021-09-30 14:46:58 +0200

    premier fichier js

---
+++ app.js
@@ -0,0 +1,11 @@
+let firstName = "jean";
+console.log(firstName);
+firstName = "jiji";
+console.log(firstName);
+firstName = "jajo";
+console.log(firstName);
+// typae dynamque
+let age = 87;
+age = "89";
+// COnstantes: ces valeurs ne peuvent pas etre modifier
+const pays = "france";
